6b186706a6e401345c139383b817e901f642d445
xaviersvd <isac.lx90@gmail.com>
2026-02-16 16:03:09 -0300

    Adicona controle de estoque

--- produto.py
+++ produto.py
@@ -4,14 +4,37 @@ class Produto:
             self.preco = preco
             self.quantidade = quantidade
       def vender(self, qtd):
-              pass
+            self.remover_estoque(qtd)
       def repor(self, qtd):
-              pass
+            self.adicionar_estoque(qtd)
       def mostrar_estoque(self):
-             print(f'''Produto: {self.nome}
+                  print(f'''Produto: {self.nome}
 preco: R${self.preco}
 Unidades em estoque: {self.quantidade}''')
+
              
       def adicionar_estoque(self, qtd):
-            self.quantidade + qtd
+            self.quantidade += qtd
             print(f'{qtd} unidades adicionadas ao estoque')
+      def remover_estoque(self, qtd):
+            if qtd > self.quantidade:
+                  print('❌ Erro: Estoque Insuficiente!!')
+            else:
+                  self.quantidade -= qtd
+                  print(f'{qtd} Unidades removidas do estoque.')
+            if self.quantidade <= 5:
+                  print('⚠️  Aviso estoque baixo!')
+
+p1 = Produto('Coca-Cola', 7.00, 5)
+p2 = Produto('Salgadinho', 5.50, 4)
+p3 = Produto('Chocolate', 6.50, 3)
+
+p2.vender(1)
+p2.mostrar_estoque()
+
+p3.vender(2)
+p3.mostrar_estoque()
+
+p3.repor(5)
+p3.mostrar_estoque()
+
